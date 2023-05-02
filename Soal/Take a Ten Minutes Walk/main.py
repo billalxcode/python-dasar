dc2eae5b3f4c9445a68516e93ad031c19fcaed2e
Billal Fauzan <71764176+billalxcode@users.noreply.github.com>
2023-05-02 11:35:16 +0000

    inisialisasi commit

---
+++ Soal/Take a Ten Minutes Walk/main.py
@@ -0,0 +1,14 @@
+# Inisialisasi
+data = ['n','s','n','s','n','s','n','s','n','s']
+
+# Metode pertama
+def metode_pertama():
+    # Looping data array yang ada di variabel "item"
+    # Kemudian disimpan ke variabel "item"
+    for item in data:
+        if data == 'w':
+            return True
+    return False
+
+metode_pertama()
+    
